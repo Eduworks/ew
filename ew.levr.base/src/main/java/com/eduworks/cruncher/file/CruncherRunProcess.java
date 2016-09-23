bacaeceef20ebbbcdc846d59b1f66451bc0fe8b1
Ronald Ray <fritz.ray@eduworks.com>
2016-09-23 06:50:02 -0700

    Extended #runProcess to use working directory and stream output from both stdout and stderr. There should be a more elegant way to merge the output of two readers than this.
    Skyrepo now doesn't return the data if it saves correctly.

--- ew.levr.base/src/main/java/com/eduworks/cruncher/file/CruncherRunProcess.java
+++ ew.levr.base/src/main/java/com/eduworks/cruncher/file/CruncherRunProcess.java
@@ -1,6 +1,7 @@
 package com.eduworks.cruncher.file;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,6 +11,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.eduworks.lang.threading.EwThreading;
 import com.eduworks.resolver.Context;
 import com.eduworks.resolver.Cruncher;
 
@@ -26,23 +28,55 @@ public class CruncherRunProcess extends Cruncher
 			args[i] = argsArray.getString(i);
 		}
 		
-		ProcessBuilder builder = new ProcessBuilder(args);
-
-        Process process;
-        ArrayList<String> output = new ArrayList<String>();
+		ArrayList<String> output = new ArrayList<String>();
+		
 		try {
+			ProcessBuilder builder = new ProcessBuilder(args);
+			String wd = optAsString("workingDirectory",null,c,parameters,dataStreams);
+			if (wd != null)
+				builder.directory(new File(wd));
+	        Process process;
+	        
 			process = builder.start();
-			
+
 			InputStream outp = process.getInputStream();
-	        InputStreamReader isr = new InputStreamReader(outp);
+			InputStreamReader isr = new InputStreamReader(outp);
 	        BufferedReader br = new BufferedReader(isr);
+			InputStream eoutp = process.getErrorStream();
+			InputStreamReader eisr = new InputStreamReader(eoutp);
+	        BufferedReader ebr = new BufferedReader(eisr);
 	        
 	        String line;
-	        process.waitFor();
-	        process.destroy();
+	        int timeout = optAsInteger("timeout", -1, c, parameters, dataStreams);
+	        if (timeout < 0)
+	        	process.waitFor();
+	        else
+	        	for (int i = 0;i < 10000;i++)
+	        	{
+	        		EwThreading.sleep(timeout/10000);
+	    	        while ((line = br.readLine()) != null) {
+	    	        	output.add(line);
+	    	        }
+	    	        while ((line = ebr.readLine()) != null) {
+	    	        	output.add(line);
+	    	        }
+	        		try
+	        		{
+	        			process.exitValue();
+	        			break;
+	        		}
+	        		catch (IllegalThreadStateException ex)
+	        		{
+	        			
+	        		}
+	        	}
 	        while ((line = br.readLine()) != null) {
-	        	output.add(line);
-	        }
+    	        	output.add(line);
+    	        }
+    	        while ((line = ebr.readLine()) != null) {
+    	        	output.add(line);
+    	        }
+	        process.destroy();
 	        
 		} catch (IOException e) {
 			e.printStackTrace();
