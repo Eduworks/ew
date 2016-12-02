63f72185004e4ca28dac83dcd5de6ab62b166941
Ronald Ray <fritz.ray@eduworks.com>
2016-12-01 16:36:09 -0800

    Now working with scripters.

--- ew.levr.core/src/main/java/com/eduworks/resolver/Scripter.java
+++ ew.levr.core/src/main/java/com/eduworks/resolver/Scripter.java
@@ -13,31 +13,47 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.eduworks.lang.EwList;
-import com.eduworks.lang.json.impl.EwJsonObject;
 import com.eduworks.resolver.exception.EditableRuntimeException;
 
-public abstract class Scripter implements Resolvable {
+public abstract class Scripter implements Resolvable
+{
 	Map<String, Object> data = null;
 	public boolean resolverCompatibilityReplaceMode = true;
 	public static Logger log = Logger.getLogger(Scripter.class);
+	protected Integer codeLineNumber = null;
+	protected Integer codeColNumber = null;
+	protected String codeFileName = null;
+	protected String codeMethod = null;
 
-	public boolean isObj(String key) {
+	public boolean isObj(String key)
+	{
 		return key.equals("obj");
 	}
 
-	protected JSONObject jo(Object...strings) throws JSONException
+	@Override
+	public void setLineAndColAndSource(Integer line, Integer col, String file, String method)
+	{
+		codeLineNumber = line;
+		codeColNumber = col;
+		codeFileName = file;
+		codeMethod = method;
+	}
+
+	protected JSONObject jo(Object... strings) throws JSONException
 	{
 		JSONObject jo = new JSONObject();
-		for (int i = 0;i < strings.length;i += 2)
+		for (int i = 0; i < strings.length; i += 2)
 		{
-			jo.put(strings[i].toString(),strings[i+1]);
+			jo.put(strings[i].toString(), strings[i + 1]);
 		}
 		return jo;
 	}
-	
-	public String toJson() throws JSONException {
+
+	public String toJson() throws JSONException
+	{
 		JSONObject jo = new JSONObject();
-		for (String key : data.keySet()) {
+		for (String key : data.keySet())
+		{
 			Object object = data.get(key);
 			if (object instanceof Resolvable)
 				jo.put(key, ((Resolvable) object).toJson());
@@ -48,9 +64,11 @@ public abstract class Scripter implements Resolvable {
 		return jo.toString();
 	}
 
-	public String toOriginalJson() throws JSONException {
+	public String toOriginalJson() throws JSONException
+	{
 		JSONObject jo = new JSONObject();
-		for (String key : data.keySet()) {
+		for (String key : data.keySet())
+		{
 			Object object = data.get(key);
 			if (object instanceof Resolvable)
 				jo.put(key, ((Resolvable) object).toOriginalJson());
@@ -58,117 +76,142 @@ public abstract class Scripter implements Resolvable {
 				jo.put(key, object);
 		}
 
-		String func = getClass().getSimpleName().replace("Resolver", "")
-				.replace("Cruncher", "").replace("Scripter", "");
+		String func = getClass().getSimpleName().replace("Resolver", "").replace("Cruncher", "").replace("Scripter", "");
 		func = "s" + Character.toLowerCase(func.charAt(0)) + func.substring(1);
 
 		jo.put("function", func);
 		return jo.toString();
 	}
 
-	public boolean has(String string) {
+	public boolean has(String string)
+	{
 		return data.containsKey(string);
 	}
 
-	public Iterator<String> sortedKeys() {
+	public Iterator<String> sortedKeys()
+	{
 		EwList<String> results = new EwList<String>(data.keySet());
 		results.sort(results);
 		return results.iterator();
 	}
 
-	public Object jsonObject(Object o) {
-		
-		if (o instanceof JSONObject) {
-			JSONObject jo = (JSONObject)o;
-			try {
+	public Object jsonObject(Object o)
+	{
+
+		if (o instanceof JSONObject)
+		{
+			JSONObject jo = (JSONObject) o;
+			try
+			{
 				JSONArray keys = jo.names();
-				for (int keyIndex=0; keyIndex<keys.length(); keyIndex++) {
+				for (int keyIndex = 0; keyIndex < keys.length(); keyIndex++)
+				{
 					Object element = jo.get(keys.getString(keyIndex));
-					if ((element instanceof JSONObject)||(element instanceof JSONArray))
+					if ((element instanceof JSONObject) || (element instanceof JSONArray))
 						jo.put(keys.getString(keyIndex), jsonObject(element));
 				}
 				jo.put("function", "object");
-			} catch (JSONException e) {
-				e.printStackTrace();
 			}
-		} else if (o instanceof JSONArray) {
-			try {
-				JSONArray array = (JSONArray) o;
-				for (int keyIndex=0; keyIndex<array.length(); keyIndex++) {
-					Object element = array.get(keyIndex);
-					if ((element instanceof JSONObject)||(element instanceof JSONArray))
-						array.put(keyIndex, jsonObject(element));
-				}
-			} catch (JSONException e) {
+			catch (JSONException e)
+			{
 				e.printStackTrace();
 			}
 		}
-		
+		else if (o instanceof JSONArray)
+		{
+			try
+			{
+				JSONArray array = (JSONArray) o;
+				for (int keyIndex = 0; keyIndex < array.length(); keyIndex++)
+				{
+					Object element = array.get(keyIndex);
+					if ((element instanceof JSONObject) || (element instanceof JSONArray))
+						array.put(keyIndex, jsonObject(element));
+				}
+			}
+			catch (JSONException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
 		return o;
 	}
 
-	public Set<String> keySet() {
+	public Set<String> keySet()
+	{
 		return data.keySet();
 	}
 
-	public Object resolve(String key, Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException {
-		try {
+	public Object resolve(String key, Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
+	{
+		try
+		{
 			JSONObject functionPack = new JSONObject();
 			functionPack.put("function", key);
 			Set<Entry<String, String[]>> parameterSet = parameters.entrySet();
 			for (Entry<String, String[]> parameter : parameterSet)
 				functionPack.put(parameter.getKey(), parameter.getValue());
 			Object o = ResolverFactory.create(functionPack);
-			if (o instanceof Cruncher) {
+			if (o instanceof Cruncher)
+			{
 				Cruncher cruncher = (Cruncher) o;
 				cruncher.resolve(c, parameters, dataStreams);
 			}
-			if (o instanceof Scripter) {
+			if (o instanceof Scripter)
+			{
 				Scripter cruncher = (Scripter) o;
 				cruncher.resolve(c, parameters, dataStreams);
 			}
 			return o;
-		} catch (EditableRuntimeException ex) {
+		}
+		catch (EditableRuntimeException ex)
+		{
 			ex.append("in " + getKeys(this));
 			throw ex;
 		}
 	}
-	
-	private Set<String> getKeys(Scripter cruncher) {
+
+	private Set<String> getKeys(Scripter cruncher)
+	{
 		return data.keySet();
 	}
 
-	public Object get(String key) {
+	public Object get(String key)
+	{
 		if (data == null)
 			return null;
 		return data.get(key);
 	}
 
-	public boolean hasParam(String key) {
+	public boolean hasParam(String key)
+	{
 		if (data.get(key) != null)
 			if (data.get(key) instanceof String)
 				return ((String) data.get(key)).startsWith("@");
 		return false;
 	}
 
-	public static boolean isSetting(String key) {
+	public static boolean isSetting(String key)
+	{
 		return Cruncher.isSetting(key);
 	}
 
-	public void build(String key, Object value) {
+	public void build(String key, Object value)
+	{
 		if (data == null)
 			data = new LinkedHashMap<String, Object>();
 		data.put(key, value);
 	}
 
-	public String getResolverName() {
-		return getClass().getSimpleName().replace("Scripter", "").toLowerCase()
-				.substring(0, 1)
-				+ getClass().getSimpleName().replace("Scripter", "")
-						.substring(1);
+	public String getResolverName()
+	{
+		return getClass().getSimpleName().replace("Scripter", "").toLowerCase().substring(0, 1)
+				+ getClass().getSimpleName().replace("Scripter", "").substring(1);
 	}
 
-	public String[] getResolverNames() {
+	public String[] getResolverNames()
+	{
 		return new String[] { "s" + getResolverName(), getResolverName() };
 	}
 }
