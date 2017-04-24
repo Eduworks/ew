842089554d894e5abb4ee04645258aeb1b6a0834
Ronald Ray <fritz.ray@eduworks.com>
2017-04-24 11:00:44 -0700

    Documentation updates.

--- docs/api/api.js
+++ docs/api/api.js
@@ -12,19 +12,32 @@ YUI.add("yuidoc-meta", function(Y) {
         "fileList",
         "fileLoad",
         "fileSave",
+        "fileSize",
+        "fileToBase64",
+        "fileToString",
+        "filename",
+        "filepath",
+        "jsonLdCompact",
+        "jsonLdExpand",
         "variableGet",
         "variableSet",
         "wsBroadcast",
         "wsEmit"
     ],
     "modules": [
-        "ew.levr.base"
+        "ew.levr.base",
+        "ew.levr.rdf"
     ],
     "allModules": [
         {
             "displayName": "ew.levr.base",
             "name": "ew.levr.base",
-            "description": "Caches a result, and fetches it automatically (without executing the code in obj) if it is in cache.<br>Cache, by default, persists over the web service request.\n\nrs2: result = obj.cache(name=\"unique name\");<br>\nLevrJS: result = cache.call(this,obj,\"unique name\");"
+            "description": "Caches a result, and fetches it automatically (without executing the code in obj) if it is in cache.<br>Cache, by default, persists over the web service\nrequest.\n\nrs2: result = obj.cache(name=\"unique name\");<br>\nLevrJS: result = cache.call(this,obj,\"unique name\");"
+        },
+        {
+            "displayName": "ew.levr.rdf",
+            "name": "ew.levr.rdf",
+            "description": "Converts a JSON object to JSON-LD and performs a Compact operation.\n\nrs2: result = obj.jsonLdCompact();<br>\nLevrJS: result = jsonLdCompact.call(this,obj);"
         }
     ],
     "elements": []
