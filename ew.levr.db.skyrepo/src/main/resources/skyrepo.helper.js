2973c2c2f72dcd49f67e5ad768ac2bd30d6d9137
devlin junker <devlin.junker@eduworks.com>
2017-08-09 09:52:42 -0700

    Fixed issues with types and indexes to return type list
    Also fixed issue in flatten string fields that was breaking elasticsearch

--- ew.levr.db.skyrepo/src/main/resources/skyrepo.helper.js
+++ ew.levr.db.skyrepo/src/main/resources/skyrepo.helper.js
@@ -1,18 +1,38 @@
 
 
-
-function flattenStringFields(jsonLd){
-
+/**
+ * Helper function to flatten any string-language objects (Objects 
+ * with @value and @lang fields only) into an elasticsearch
+ * friendly type
+ *  
+ * @param jsonLd object to flatten
+ * @returns flattened jsonLd object as String
+ */
+// @first is an internal parameter to indicate which call is not a recursive call (used to return a string object at the end)
+function flattenStringFields(jsonLd, first){
+	
 	if(jsonLd == undefined){
 		jsonLd = JSON.parse(this.params.obj);
+		first = true;
 	}
-	
-	if(jsonLd === Object(jsonLd)){
+
+
+	if(Array.isArray(jsonLd)){
+		var ret = [];
+		
+		for(idx in jsonLd){
+			ret.push(flattenStringFields(jsonLd[idx], false));
+		}
+		
+		return ret;
+	}else if(jsonLd === Object(jsonLd)){
 		for(var key in jsonLd){
 			try{
 				var field = jsonLd[key];
+
+				// Check if objects in array are string-language objects
 				if(Array.isArray(field)){
-					var flattenText = true;
+					var flattenText = true; // flag identifying if they are string-language objects
 					var textArray = [];
 					var langArray = [];
 					for(var idx in field){
@@ -29,18 +49,23 @@ function flattenStringFields(jsonLd){
 							flattenText = false;
 						}
 					}
+					// if not string-language objects keep recursing
 					if(!flattenText){
-						jsonLd[key] = flattenStringFields(field);
+						jsonLd[key] = flattenStringFields(field, false);
+					// otherwise flatten @value to field and create <key>@language field
 					}else{
 						jsonLd[key] = textArray;
 						jsonLd[key+"@language"] = langArray;
 					}	
+				// Check if object is a string-language object
 				}else if(field === Object(field)){
+					// if string-language object flatten @value to field and create <key>@language field
 					if(Object.keys(field).length == 2 && field["@value"] != undefined && field["@language"] != undefined){
 						jsonLd[key] = field["@value"];
 						jsonLd[key+"@language"] = field["@language"];
+					// otherwise keep recursing
 					}else if (Object.keys(field).length > 0){
-						jsonLd[key] = flattenStringFields(field);
+						jsonLd[key] = flattenStringFields(field, false);
 					}	
 				}
 			}catch(exception){
@@ -48,5 +73,10 @@ function flattenStringFields(jsonLd){
 			}
 		}
 	}
-	return jsonLd;
+	
+	if(first != undefined && first == false){
+		return jsonLd;
+	}else{
+		return JSON.stringify(jsonLd)
+	}
 }
