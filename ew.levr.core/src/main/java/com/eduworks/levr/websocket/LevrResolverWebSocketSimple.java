a2bb343fbf4fd82da911313795b1bf3404234fd7
Ronald Ray <fritz.ray@eduworks.com>
2017-08-17 10:26:10 -0700

    Added simpler web socket endpoint.

---
+++ ew.levr.core/src/main/java/com/eduworks/levr/websocket/LevrResolverWebSocketSimple.java
@@ -0,0 +1,10 @@
+package com.eduworks.levr.websocket;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by fray on 8/17/17.
+ */
+@ServerEndpoint(value = "/ws")
+public class LevrResolverWebSocketSimple extends LevrResolverWebSocket {
+}
