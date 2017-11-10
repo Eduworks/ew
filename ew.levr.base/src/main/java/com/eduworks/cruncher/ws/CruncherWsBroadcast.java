3a4a1ede61b7d3b9636ded2c59e21ad1c89e708e
Ronald Ray <fritz.ray@eduworks.com>
2017-11-10 13:46:35 -0800

    Async remote calls can fail if in a bad state. Retry till it works.

--- ew.levr.base/src/main/java/com/eduworks/cruncher/ws/CruncherWsBroadcast.java
+++ ew.levr.base/src/main/java/com/eduworks/cruncher/ws/CruncherWsBroadcast.java
@@ -1,17 +1,19 @@
 package com.eduworks.cruncher.ws;
 
+import com.eduworks.lang.threading.EwThreading;
 import com.eduworks.levr.websocket.LevrResolverWebSocket;
 import com.eduworks.resolver.Context;
 import com.eduworks.resolver.Cruncher;
-import java.io.InputStream;
-import java.util.Map;
-import javax.websocket.Session;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import javax.websocket.Session;
+import java.io.InputStream;
+import java.util.Map;
+
 /**
  * Broadcasts a message to all websockets connected to this machine.
- * 
+ * <p>
  * rs2: obj.wsBroadcast();<br>
  * LevrJS: wsBroadcast.call(this,obj);
  *
@@ -19,48 +21,51 @@ import org.json.JSONObject;
  * @module ew.levr.base
  * @author fritz.ray@eduworks.com
  */
+
 /**
  * @method wsBroadcast
  * @param obj {Cruncher|Function|Object} Thing to broadcast. Will be converted to a string.
  * @return {Object} Returns obj.
  */
-public class CruncherWsBroadcast extends Cruncher
-{
+public class CruncherWsBroadcast extends Cruncher {
 
-    @Override
-    public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
-    {
-        Object obj = getObj(c, parameters, dataStreams);
-        if (obj == null) return null;
-        for (Session s : LevrResolverWebSocket.sessions)
-        {
-            s.getAsyncRemote().sendText(obj.toString());
-        }
-        return obj;
-    }
+	@Override
+	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException {
+		Object obj = getObj(c, parameters, dataStreams);
+		if (obj == null) return null;
+		for (Session s : LevrResolverWebSocket.sessions) {
+			boolean done = false;
+			int counter = 0;
+			while (!done && counter < 1000) {
+				try {
+					s.getAsyncRemote().sendText(obj.toString());
+					done = true;
+				} catch (Exception ex) {
+					EwThreading.sleep(5);
+				}
+			}
+		}
+		return obj;
+	}
 
-    @Override
-    public String getDescription()
-    {
-        return "Broadcasts a message to all websockets connected to this machine.";
-    }
+	@Override
+	public String getDescription() {
+		return "Broadcasts a message to all websockets connected to this machine.";
+	}
 
-    @Override
-    public String getReturn()
-    {
-        return "Object";
-    }
+	@Override
+	public String getReturn() {
+		return "Object";
+	}
 
-    @Override
-    public String getAttribution()
-    {
-        return ATTRIB_NONE;
-    }
+	@Override
+	public String getAttribution() {
+		return ATTRIB_NONE;
+	}
 
-    @Override
-    public JSONObject getParameters() throws JSONException
-    {
-        return jo("obj","Object");
-    }
-    
+	@Override
+	public JSONObject getParameters() throws JSONException {
+		return jo("obj", "Object");
+	}
+
 }
