10930a90533e65ed2629612802b42033929132f7
Ronald Ray <fritz.ray@eduworks.com>
2018-02-26 06:58:05 -0800

    cassproject/CASS#31 No longer revives deleted content.

--- ew.levr.db.skyrepo/src/main/resources/skyrepo.backup.js
+++ ew.levr.db.skyrepo/src/main/resources/skyrepo.backup.js
@@ -2,6 +2,8 @@ skyrepoBackup = function(){
 	if (this.params.secret != skyIdSecret())
 		error("You must provide secret=`cat skyId.secret` to invoke backup.",401);
 	var backup = {};
+	backup.permanent = {};
+	backup.indexed = {};
 	var settings = elasticMapping();
 	var indices = EcObject.keys(settings);
 	var types = [];
@@ -12,13 +14,20 @@ skyrepoBackup = function(){
 		var keys = idxKeys(".","skyrepo",types[i]);
 		for (var j = 0;j < keys.length;j++)
 		{
-			backup[types[i]+"/"+keys[j]]=JSON.parse(idxGet(".","skyrepo",types[i],keys[j]));
+			backup.permanent[types[i]+"/"+keys[j]]=JSON.parse(idxGet(".","skyrepo",types[i],keys[j]));
 		}
 	}
 
-	var firstQueryPost = {query:{query_string:{query:"*:*"}},explain:"false",size:"50",sort:"_doc"};
+	var firstQueryPost = {
+		query:{
+			query_string:{query:"*:*"}
+		},
+		explain:"false",
+		size:"50",
+		sort:"_doc"
+	};
 	var firstQueryUrl = urlBase() + "/_search?scroll=1m&version";
-
+	console.log(firstQueryUrl);
 	var results = httpPost(JSON.stringify(firstQueryPost),firstQueryUrl,"application/json","false");
 	var scroll = results["_scroll_id"];
 	while (results != null && scroll != null && scroll != "")
@@ -30,8 +39,8 @@ skyrepoBackup = function(){
 		for (var i = 0;i < hits.length;i++)
 		{
 			var key = hits[i]["_type"]+"/"+hits[i]["_id"]+"/"+hits[i]["_version"];
-			if (backup[key] == null)
-				backup[key]=hits[i]["_source"];
+			if (backup.indexed[key] == null)
+				backup.indexed[key]=hits[i]["_source"];
 		}
 		results = httpGet(urlBase() + "/_search/scroll?scroll=1m&scroll_id="+scroll);
 	}
@@ -50,11 +59,18 @@ skyrepoRestore = function(){
 	if (EcArray.isArray(file))
 		file = file[0];
 	file = JSON.parse(fileToString(file));
-	var keys = EcObject.keys(file);
+	var keys = EcObject.keys(file.indexed);
 	for (var i = 0;i < keys.length;i++)
 	{
 		var parts = keys[i].split("/");
-		log.push(skyrepoPutInternal({"obj":JSON.stringify(file[keys[i]]), type:parts[0], id:parts[1], version:parts[2]}));
+		log.push(skyrepoPutInternalIndex({"obj":JSON.stringify(file.indexed[keys[i]]), type:parts[0], id:parts[1], version:parts[2]}));
+	}
+	keys = EcObject.keys(file.permanent);
+	for (var i = 0;i < keys.length;i++)
+	{
+		var parts = keys[i].split("/");
+		skyrepoPutInternalTypeCheck({"obj":JSON.stringify(file.permanent[keys[i]]), type:parts[0], id:parts[1], version:parts[2]});
+		log.push(skyrepoPutInternalPermanent({"obj":JSON.stringify(file.permanent[keys[i]]), type:parts[0], id:parts[1], version:parts[2]}));
 	}
 	return JSON.stringify(log,null,2);
 }
