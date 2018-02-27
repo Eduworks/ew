4e3fe10529e220c8a254f5f9bb6364d59e37af54
Ronald Ray <fritz.ray@eduworks.com>
2018-02-27 09:21:22 -0800

    Deactivated test that requires credentials.

--- ew.levr.service.dropbox/src/test/java/DropboxTest.java
+++ ew.levr.service.dropbox/src/test/java/DropboxTest.java
@@ -10,20 +10,20 @@ import java.util.HashMap;
 
 
 public class DropboxTest {
-    @Test
-    public void ListTest() throws JSONException {
-
-        CruncherDropboxList cdl = new CruncherDropboxList();
-        cdl.build("path","");
-        cdl.build("accessToken","");
-        cdl.build("clientIdentifier","dropbox/eduworks_test");
-        JSONArray result = (JSONArray) cdl.resolve(new Context(),new HashMap<>(),null);
-        System.out.println(result.toString());
-        CruncherDropboxLoad l = new CruncherDropboxLoad();
-        l.build("path",result.getString(0));
-        l.build("accessToken","");
-        l.build("clientIdentifier","dropbox/eduworks_test");
-        InMemoryFile imf = (InMemoryFile) l.resolve(new Context(),new HashMap<>(),null);
-        System.out.println(imf.data.length);
-    }
+//    @Test
+//    public void ListTest() throws JSONException {
+//
+//        CruncherDropboxList cdl = new CruncherDropboxList();
+//        cdl.build("path","");
+//        cdl.build("accessToken","");
+//        cdl.build("clientIdentifier","dropbox/eduworks_test");
+//        JSONArray result = (JSONArray) cdl.resolve(new Context(),new HashMap<>(),null);
+//        System.out.println(result.toString());
+//        CruncherDropboxLoad l = new CruncherDropboxLoad();
+//        l.build("path",result.getString(0));
+//        l.build("accessToken","");
+//        l.build("clientIdentifier","dropbox/eduworks_test");
+//        InMemoryFile imf = (InMemoryFile) l.resolve(new Context(),new HashMap<>(),null);
+//        System.out.println(imf.data.length);
+//    }
 }
