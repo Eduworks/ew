31f8b5b023297e075bbed3b1aa0108bb5c996196
Ronald Ray <fritz.ray@eduworks.com>
2018-03-27 09:12:11 -0700

    AMQP Crunchers added to levr.net

---
+++ ew.levr.net/src/main/java/com/eduworks/cruncher/amqp/CruncherAmqpListen.java
@@ -0,0 +1,102 @@
+package com.eduworks.cruncher.amqp;
+
+import com.eduworks.levr.servlet.impl.LevrResolverServlet;
+import com.eduworks.resolver.Context;
+import com.eduworks.resolver.Cruncher;
+import com.eduworks.resolver.Resolvable;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CruncherAmqpListen extends Cruncher {
+	@Override
+	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException {
+		String hostName = optAsString("hostName", "localhost", c, parameters, dataStreams);
+		Integer port = optAsInteger("port", -1, c, parameters, dataStreams);
+		String queueName = optAsString("queue","", c, parameters, dataStreams);
+		String exchangeName = optAsString("exchangeName","",c,parameters,dataStreams);
+		String exchangeType = optAsString("exchangeType","topic",c,parameters,dataStreams);
+		String routingKey = optAsString("routingKey","", c, parameters, dataStreams);
+		boolean durable = optAsBoolean("durable", false, c, parameters, dataStreams);
+		boolean exclusive = optAsBoolean("exclusive", false, c, parameters, dataStreams);
+		boolean autoDelete = optAsBoolean("autoDelete", false, c, parameters, dataStreams);
+
+		Channel channel = CruncherAmqpSend.getChannel(hostName, port, queueName, exchangeName,exchangeType,routingKey,durable, exclusive, autoDelete);
+		final Resolvable op = (Resolvable) get("obj");
+		try {
+			channel.basicConsume(queueName, true, new DefaultConsumer(channel) {
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+					Context c = new Context();
+					try {
+						if (body == null)
+							return;
+						Map<String, String[]> newParameters = new HashMap<String, String[]>(parameters);
+						newParameters.put("body", new String[]{new String(body)});
+						newParameters.put("consumerTag", new String[]{consumerTag});
+						newParameters.put("routingKey", new String[]{envelope.getRoutingKey()});
+						newParameters.put("exchange", new String[]{envelope.getExchange()});
+						newParameters.put("deliveryTag", new String[]{Long.toString(envelope.getDeliveryTag())});
+						newParameters.put("contentEncoding", new String[]{properties.getContentEncoding()});
+						newParameters.put("contentType", new String[]{properties.getContentType()});
+						newParameters.put("type", new String[]{properties.getType()});
+						newParameters.put("appId", new String[]{properties.getAppId()});
+						newParameters.put("clusterId", new String[]{properties.getClusterId()});
+						newParameters.put("userId", new String[]{properties.getUserId()});
+						newParameters.put("correlationId", new String[]{properties.getCorrelationId()});
+						newParameters.put("expiration", new String[]{properties.getExpiration()});
+						newParameters.put("replyTo", new String[]{properties.getReplyTo()});
+						newParameters.put("className", new String[]{properties.getClassName()});
+						newParameters.put("messageId", new String[]{properties.getMessageId()});
+						newParameters.put("classId", new String[]{Integer.toString(properties.getClassId())});
+						log.debug(consumerTag + " Received: " + body);
+						LevrResolverServlet.initConfig(System.out, null);
+						((Resolvable) op).resolve(c, newParameters, dataStreams);
+						c.success();
+					} catch (Throwable e) {
+						c.failure();
+						if (!(e instanceof RuntimeException))
+							e.printStackTrace();
+						else if (e.getMessage() != null && !e.getMessage().isEmpty())
+							System.out.println(e.getMessage());
+					} finally {
+						c.finish();
+					}
+					super.handleDelivery(consumerTag, envelope, properties, body);
+				}
+			});
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Listens for AMQP messages and executes 'obj'. Payload is in @body.";
+	}
+
+	@Override
+	public String getReturn() {
+		return null;
+	}
+
+	@Override
+	public String getAttribution() {
+		return ATTRIB_NONE;
+	}
+
+	@Override
+	public JSONObject getParameters() throws JSONException {
+		return jo("obj","Resolvable","hostname","String","port","Integer","queue","String","?durable","Boolean","?exclusive","Boolean","?autoDelete","Boolean");
+
+	}
+}
---
+++ ew.levr.net/src/main/java/com/eduworks/cruncher/amqp/CruncherAmqpSend.java
@@ -0,0 +1,114 @@
+package com.eduworks.cruncher.amqp;
+
+import com.eduworks.resolver.Context;
+import com.eduworks.resolver.Cruncher;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+public class CruncherAmqpSend extends Cruncher {
+
+	public static Map<String, ConnectionFactory> factories = new HashMap<>();
+	public static Map<ConnectionFactory, Connection> connections = new HashMap<>();
+	public static Map<String, Channel> channels = new HashMap<>();
+
+	@Override
+	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException {
+		String hostName = optAsString("hostName", "localhost", c, parameters, dataStreams);
+		Integer port = optAsInteger("port", -1, c, parameters, dataStreams);
+		String queueName = optAsString("queue","", c, parameters, dataStreams);
+		String routingKey = optAsString("routingKey","", c, parameters, dataStreams);
+		String exchangeName = optAsString("exchangeName","",c,parameters,dataStreams);
+		String exchangeType = optAsString("exchangeType","topic",c,parameters,dataStreams);
+		boolean durable = optAsBoolean("durable", false, c, parameters, dataStreams);
+		boolean exclusive = optAsBoolean("exclusive", false, c, parameters, dataStreams);
+		boolean autoDelete = optAsBoolean("autoDelete", false, c, parameters, dataStreams);
+		Object o = getObj(c, parameters, dataStreams);
+
+		Channel channel = getChannel(hostName, port, queueName, exchangeName,exchangeType,routingKey,durable, exclusive, autoDelete);
+		try {
+			channel.basicPublish(exchangeName, exchangeName.isEmpty() ? queueName : routingKey, null, o.toString().getBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return null;
+	}
+
+	public static Channel getChannel(String hostName, Integer port, String queueName, String exchangeName,String exchangeType,String routingKey,boolean durable, boolean exclusive, boolean autoDelete) {
+		ConnectionFactory factory = null;
+		synchronized (factories) {
+			if (factories.containsKey(hostName))
+				factory = factories.get(hostName);
+			else {
+				factory = new ConnectionFactory();
+				factory.setHost(hostName);
+				if (port != null)
+					factory.setPort(port);
+				factories.put(hostName, factory);
+			}
+		}
+		Connection connection = null;
+		synchronized (connections) {
+			if (connections.containsKey(factory))
+				connection = connections.get(factory);
+			else {
+				try {
+					connection = factory.newConnection();
+					connections.put(factory, connection);
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				} catch (TimeoutException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		String cacheName = hostName + port + queueName+":"+exchangeName+exchangeType+routingKey + durable + exclusive + autoDelete;
+		Channel channel = null;
+		synchronized (channels) {
+			try {
+				if (!channels.containsKey(cacheName)) {
+					channel = connection.createChannel();
+					if (!queueName.isEmpty())
+					channel.queueDeclare(queueName, durable, exclusive, autoDelete, null);
+					if (!exchangeName.isEmpty())
+						channel.exchangeDeclare(exchangeName,exchangeType);
+					if (!queueName.isEmpty() && !exchangeName.isEmpty())
+						channel.queueBind(queueName,exchangeName,routingKey);
+					channels.put(cacheName, channel);
+				} else
+					channel = channels.get(cacheName);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return channel;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Sends an AMQP Message. May only work with RabbitMQ. RIP EDI.";
+	}
+
+	@Override
+	public String getReturn() {
+		return null;
+	}
+
+	@Override
+	public String getAttribution() {
+		return ATTRIB_NONE;
+	}
+
+	@Override
+	public JSONObject getParameters() throws JSONException {
+		return jo("obj","Object","hostname","String","port","Integer","queue","String","?durable","Boolean","?exclusive","Boolean","?autoDelete","Boolean");
+	}
+}
